9ec9b248fcd49977681b3130e303d57fbac7dae9
malavdasa71-tech <malavdasa71@gmail.com>
2025-11-29 22:07:37 +0530

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['i.scdn.co', 'i1.sndcdn.com', 'img.youtube.com'],
+  },
+}
+
+module.exports = nextConfig
